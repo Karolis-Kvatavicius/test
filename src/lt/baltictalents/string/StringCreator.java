e9e8c9efe369dfff3116df60f3e11d68608bb6fd
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-07-30 19:54:24 +0300

    deleted unneeded if statements in StringCreator methods

--- src/lt/baltictalents/string/StringCreator.java
+++ src/lt/baltictalents/string/StringCreator.java
@@ -3,16 +3,10 @@ package lt.baltictalents.string;
 public class StringCreator {
 
    String create(String s1, String s2) {
-       if (s1 == null || s2 == null) {
-            return s1 + s2;
-       }
        return s1 + s2;
    }
 
    String create(String s1, String s2, String s3) {
-       if (s1 == null || s2 == null || s3 == null) {
-           return s1 + s2 + s3;
-       }
       return s1 + s2 + s3;
    }
 }
